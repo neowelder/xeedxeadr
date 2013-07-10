205806a944a6ab36e08a8eac37408bfbd0ad2b22
Agustin Ramos Escalante <aptolabt@gmail.com>
2013-07-10 12:59:53 +0200

    primera version

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace xeedxead
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.urlbox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.articulos = new System.Windows.Forms.TreeView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Blog : ";
+            // 
+            // urlbox
+            // 
+            this.urlbox.Location = new System.Drawing.Point(112, 19);
+            this.urlbox.Name = "urlbox";
+            this.urlbox.Size = new System.Drawing.Size(281, 20);
+            this.urlbox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(423, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Conectar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // articulos
+            // 
+            this.articulos.Location = new System.Drawing.Point(54, 68);
+            this.articulos.Name = "articulos";
+            this.articulos.Size = new System.Drawing.Size(444, 341);
+            this.articulos.TabIndex = 3;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 446);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(557, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 468);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.articulos);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.urlbox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "XeedXeadr";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox urlbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TreeView articulos;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.ServiceModel.Syndication;
+namespace xeedxead
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+//            string url = "http://evilnapsis.blogspot.com/feeds/posts/default";
+        }
+
+        public SyndicationFeed loadurl(string url) {
+            XmlReader reader = XmlReader.Create(url);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            reader.Close();
+            return feed;
+        }
+        
+        public void getfeed(SyndicationFeed feed) {
+            foreach (SyndicationItem item in feed.Items)
+            {
+                if (titulo != null) {
+                    TreeNode post = new TreeNode();
+                    TreeNode summary = new TreeNode();
+                    post.Text = item.Title.Text;
+                    summary.Text = item.Content.ToString();
+                    post.Nodes.Add(summary);
+                    titulo.Nodes.Add(post);
+
+                }
+            }
+        }
+
+        TreeNode titulo;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(urlbox.Text!=""){
+                SyndicationFeed feed = loadurl(urlbox.Text);
+                if (feed != null) {
+                    titulo = new TreeNode();
+                    titulo.Text = feed.Title.Text;
+                    articulos.Nodes.Add(titulo);
+                    getfeed(feed);
+                }
+            }
+        }
+    }
+}
